49be46dc42389385f8305bdfed0cd9f3cabe4f97
v3nom-95 <goslingstark95@gmail.com>
2025-12-14 12:51:45 +0530

    pics update

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -40,7 +40,7 @@ const events: EventsData = {
         {
             title: 'Zero to Live: Website Creation and Deployment for II and III Year Students',
             date: '18th - 21st August 2025 and July 28 to July 30, 2025',
-            description: 'A hands-on workshop for III Year students to learn website creation and deployment.',
+            description: 'A hands-on workshop for II & III Year students to learn website creation and deployment.',
             images: ['/w31.jpeg', '/w32.jpeg', '/w33.jpeg', '/w34.jpeg', '/w35.jpeg'],
         },
         {
