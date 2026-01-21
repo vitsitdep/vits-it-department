c0a046dd12285b18cc2436f5af4776fb6e6d81ac
v3nom-95 <goslingstark95@gmail.com>
2026-01-21 14:49:23 +0000

    hiilp

--- src/pages/Faculty.tsx
+++ src/pages/Faculty.tsx
@@ -24,7 +24,7 @@ const Faculty = () => {
       specialization: "Data Mining, Machine Learning",
       email: "bvchowdary2003@gmail.com",
       publications: "Scopus Journals & Conferences: 13 \n Patents:2",
-      awards: "Yuva Acharya Award - 2021\n\n NPTEL Discipline Star",
+      awards: "Yuva Acharya Award\n\n2021 NPTEL Discipline Star",
       image: "/BV-IT-HOD.jpg",
       portfolio: "https://bvchowdary.vercel.app" // Add portfolio links here
     },
