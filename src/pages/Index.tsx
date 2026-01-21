c89561895942c1c71d3838942615433ea8f7d482
v3nom-95 <goslingstark95@gmail.com>
2026-01-21 17:15:28 +0000

    sh

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -63,7 +63,7 @@ const Index = () => {
                 Placement Insights
               </h3>
               <img 
-                src="/pr.jpeg" 
+                src="/public/ChatGPT Image Jan 21, 2026, 10_44_38 PM.png" 
                 alt="IT Department" 
                 className="rounded-lg shadow-2xl w-full h-auto transform-gpu will-change-auto"
               />
